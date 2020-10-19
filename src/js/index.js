d770996300799bc11c00a2103051ed9edcf37958
salt-dev <salt-dev@host-78-65-176-55.homerun.telia.com>
2020-10-19 22:04:37 +0200

    fetching images

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,44 @@
 import '../styles/main.scss';
-// import img from 'infinity.png'
+
+const flipCards = document.querySelectorAll('.flipcard-front');
+console.log(flipCards)
+
+flipCards.forEach(element => {
+  console.log(element)
+});
+
+fetch(`https://api.unsplash.com/photos/random?count=${flipCards.length}&client_id=zniu4K8oGgiANYwLy20uLMtdphNxjUpHAbnWT26F96s`)
+.then(response => response.json())
+.then(parsed => {
+  const urls = parsed.map(el => el.urls.small);
+  flipCards.forEach((card, i) => {
+    const imageElement = document.createElement('img');
+    imageElement.src = urls[i];
+    imageElement.style.width = '100%';
+    imageElement.style.height = '100%';
+    imageElement.style.objectFit = 'cover';
 
 
-// const image = window.document.querySelector('img')
+    card.append(imageElement)
+  })
+})
 
-// image.src = img
+
+
+// fetch("https://api.unsplash.com/photos/random?client_id=zniu4K8oGgiANYwLy20uLMtdphNxjUpHAbnWT26F96s")
+// .then(response => response.json())
+// .then(parsed => parsed.urls.thumb)
+// .then(url => {
+//   // const flipCard = document.querySelector('.flipcard-front');
+//   const imageElement = document.createElement('img');
+//   imageElement.src = url;
+//   flipCard.append(imageElement)
+// });
+
+// .then(parsed => {
+//   console.log(parsed)
+//   const flipcard = document.querySelector('.flipcard-front');
+
+//   cardText.textContent = JSON.stringify(parsed);
+// })
+
